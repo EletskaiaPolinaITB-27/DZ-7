9b9d8d320b16c2aa20cd0c794bfdec6d3e5d7516
Polina Eletskaia <polina.e@compsscollege.art>
2026-02-28 15:53:41 +0600

    types

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface ICountryShort {
+    name: string;
+    alpha3Code: string;
+    independent: boolean
+}
+export interface ICountry {
+    name: string;
+    capital: string;
+    population: number;
+    flag: string;
+    borders?: string[]
+}
