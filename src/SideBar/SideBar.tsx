a5f5daaaf4f298b45e211b853d5615e1085e7976
Polina Eletskaia <polina.e@compsscollege.art>
2026-02-28 16:17:48 +0600

    sidebar

---
+++ src/SideBar/SideBar.tsx
@@ -0,0 +1,31 @@
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import type { ICountryShort } from '../types'; 
+
+interface Props {
+    countries: ICountryShort[];
+    onSelect: (code: string) => void;
+}
+
+
+export const Sidebar = ({countries, onSelect}:Props) => {
+    return (
+    <Drawer open
+        variant="permanent" 
+        anchor="left" 
+        PaperProps={{ sx: { width: 250}}}>
+        
+        <List>
+        {countries.map((country) => (
+            <ListItemButton key={country.alpha3Code} onClick={() => onSelect(country.alpha3Code)}>
+            <ListItemText primary={country.name}/>
+            </ListItemButton> ))
+        }
+
+        
+      </List>
+    </Drawer>
+)
+}
